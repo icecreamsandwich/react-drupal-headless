fc7abfa6af4245af68e92c399576958d2462a306
Muneeb <muneeb.k@piserve.com>
2019-05-29 21:29:54 +0530

    resolve task variables

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,8 @@ app.post('/drupal/api/getAllServiceNames', function (req, res) {
 
 });
 
-//Camunda API's
+//Camunda API's //
+//get all process instances
 app.post('/camunda/getProcessInstances', function (req, res) {
     var host = process.env.REACT_APP_CAMUNDA_URL + ':8080';
     var options = {
@@ -86,14 +87,14 @@ app.post('/camunda/tasks', function (req, res) {
 
 });
 
-// camunda tasks variables
+//camunda tasks variables
 app.post('/camunda/task_details/:task_id', function (req, res) {
     var task_id = req.params.task_id;
     //console.log(task_id);
     var host = process.env.REACT_APP_CAMUNDA_URL + ':8080';
     var options = {
         method: 'GET',
-        uri: host + '/engine-rest/task/'+task_id+'/variables',
+        uri: host + '/engine-rest/task/' + task_id + '/variables',
         headers: {
             'Content-type': 'application/json'
         }
@@ -105,18 +106,61 @@ app.post('/camunda/task_details/:task_id', function (req, res) {
 
 });
 
-//Get camunda task variables resolve
+//Resolve camunda task variables
 app.post('/camunda/task/resolve/:task_id', function (req, res) {
     var task_id = req.params.task_id;
-    console.log(task_id);
+    var jsonVars = JSON.parse(req.body.json);
+    console.log(JSON.stringify(jsonVars))
+    var variables_ar = {
+        "variables": {}
+    };
+    jsonVars.map((item, index) => {
+        var splitted = item.split(":")
+        var key = splitted[0];
+        var value = splitted[1];
+        variables_ar.variables[key] =
+            {
+                "value": value.toString(),
+                "type": "String"    
+
+            }
+    })
+    console.log(JSON.stringify(variables_ar))
+    /* var buildStr = [];
+    var variables = {};
+
+    jsonVars.map((item, index) => {
+        /* var splitter = item.split(":");
+        var jsonStr = splitter[0]+': { "value": '+splitter[1]+', "type": "String" }'
+        //buildStr['variables'].push(jsonStr);
+        buildStr[0]['variables'].push({
+            //variables : splitter[0]+': { "value": '+splitter[1]+', "type": "String" }'
+            jsonStr
+        }) 
+        var splitter = item.split(":");
+        var keyVal = splitter[0];
+        var valueVal = splitter[1];
+     
+        variables = {
+            variables : {
+                key : keyVal,
+                Val : valueVal
+            }
+        }
+        buildStr.push(variables);
+    })
+    console.log(JSON.stringify(buildStr))
+
     var host = process.env.REACT_APP_CAMUNDA_URL + ':8080';
-    auth = "Basic " + new Buffer(process.env.REACT_APP_CAMUNDA_USER + ":" + process.env.REACT_APP_CAMUNDA_PASS).toString("base64");
-    var json = {
+    auth = "Basic " + new Buffer(process.env.REACT_APP_CAMUNDA_USER + ":" + process.env.REACT_APP_CAMUNDA_PASS).toString("base64"); */
+    /* var json = {
         "variables":
         {
             "reactVariable2": { "value": "reactVariable23444", "type": "String" },
         }
-    };
+    }; */
+    var host = process.env.REACT_APP_CAMUNDA_URL + ':8080';
+    auth = "Basic " + new Buffer(process.env.REACT_APP_CAMUNDA_USER + ":" + process.env.REACT_APP_CAMUNDA_PASS).toString("base64");
     var options = {
         method: 'POST',
         uri: host + '/engine-rest/task/' + task_id + '/resolve',
@@ -124,23 +168,12 @@ app.post('/camunda/task/resolve/:task_id', function (req, res) {
             'Content-type': 'application/json',
             'Authorization': auth
         },
-        json : json
+        json: JSON.stringify(variables_ar)
     }
     request(options, function (err, response, body) {
         if (err) console.log(err);
         else res.send(body);
     })
-       /*  request.post({
-            uri: host + '/engine-rest/task/' + task_id + '/resolve',
-            headers : {
-                'Content-type': 'application/json',
-                'Authorization': auth
-            },
-            json: json
-        }, function(err, response, body){
-            if(err) console.log(err)
-            else res.send(body)
-        }) */
 });
 
 app.listen(port, function (req, res) {
--- src/components/children/camunda/TaskDetails.js
+++ src/components/children/camunda/TaskDetails.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import Tasks from './Tasks';
 
 export default class TaskDetails extends Component {
+    state = {
+        // additionalVars: []
+    }
     constructor(props) {
         super(props);
         var task_details = this.props.location.state;
@@ -10,6 +13,7 @@ export default class TaskDetails extends Component {
             this.state = {
                 task_id: task_details.task_id || '',
                 processInstanceId: task_details.processInstanceId || '',
+                additionalVars: []
             };
         }
     }
@@ -17,35 +21,83 @@ export default class TaskDetails extends Component {
     componentDidMount() {
         //axios post to get the task variables
         var host = process.env.REACT_APP_HOST_URL + ":3002";
-        if(this.state && this.state.task_id){
+        if (this.state && this.state.task_id) {
             axios.post(host + '/camunda/task_details/' + this.state.task_id)
-            .then(responseData => {
-                this.setState({
-                    taskVariables: responseData.data
-                });
-            })
-            .catch(err => console.log(err))
-        }       
+                .then(responseData => {
+                    this.setState({
+                        taskVariables: responseData.data
+                    });
+                })
+                .catch(err => console.log(err))
+        }
     }
 
-    handleChange = (e) => {
-        var name = e.target.name;
+    addVariable(e) {
+        e.preventDefault();
         this.setState({
-        [name]: e.target.value,
+            additionalVars: [...this.state.additionalVars, ""]
+        })
+    }
+    handleKeyChange(e, index) {
+        var keyItem = e.target.value;
+        var currState = this.state.additionalVars;
+        currState[index] = keyItem;
+        this.setState({
+            additionalVars: currState
+        });
+        console.log(this.state.additionalVars);
+    }
+
+    handleValueChange(e, index) {
+        var valueItem = e.target.value;
+        var currState = this.state.additionalVars;
+        currState[index] = currState[index] + ":" + valueItem;
+        this.setState({
+            additionalVars: currState
+        });
+        console.log(this.state.additionalVars);
+    }
+
+    removeVariable(e, index) {
+        e.preventDefault();
+        this.state.additionalVars.splice(index, 1);
+        this.setState({
+            additionalVars : this.state.additionalVars
         });
     }
-    handleSubmit = e => {
+
+    handleSubmit(e) {
         e.preventDefault();
         var host = process.env.REACT_APP_HOST_URL + ":3002";
-        
-        if(this.state.task_id){
-            axios.post(host + '/camunda/task/resolve/' + this.state.task_id)
-            .then(responseData => {
-              //  if(responseData) alert("Variables Successfully updated")
-                alert("Variables Successfully updated")
+        var json = JSON.stringify(this.state.additionalVars);
+        //var json = JSON.stringify(this.state.additionalVars);
+        //console.log(json);
+        //build the json string
+        /* var varString = ""
+        this.state.additionalVars.map((item, index) => {
+            return(
+                varString = index+': { "value": '+item+', "type": "String" }'
+            )
+        })
+        var jsonString = {
+            "variables":
+            {
+                varString
+                //"reactVariable2": { "value": "reactVariable23444", "type": "String" },
+            }
+        }; 
+        console.log(jsonString);    
+        */
+        if (this.state.task_id) {
+            axios.post(host + '/camunda/task/resolve/' + this.state.task_id, {
+                json: json
             })
-            .catch(err => console.log(err))
-        }       
+                .then(responseData => {
+                    if (responseData) alert("Variables Successfully updated")
+                    //  alert("Variables Successfully updated")
+                })
+                .catch(err => console.log(err))
+        }
     }
     render() {
         if (!this.state || !this.state.taskVariables) {
@@ -54,49 +106,61 @@ export default class TaskDetails extends Component {
         return (
             <div>
                 <h2>Task Variables</h2>
-                <form
-                    action=""
-                    method="POST"
-                    onSubmit={this.handleSubmit}
-                    className="form"
-                >
-                    <table className="table">
-                        <tr>
-                            <td> network_sec_type</td>
-                            <td>
-                                <input
-                                    type="text"
-                                    name="network_sec_type"
-                                    value={this.state.taskVariables.network_sec_type.value}
-                                    onChange={e => {
-                                        this.handleChange(e);
-                                    }}
-                                    readOnly="readonly"
-                                />
-                            </td>
-                        </tr>                       
-                        <tr>
-                            <td> node_status</td>
-                            <td>
-                                <input
-                                    type="text"
-                                    name="node_status"
-                                    value={this.state.taskVariables.node_status.value}
-                                    onChange={e => {
-                                        this.handleChange(e);
-                                    }}
-                                    readOnly="readonly"
-                                />
-                            </td>
-                        </tr>                       
-                        <tr>
-                            <td />
-                            <td>
-                                <input type="submit" value="Submit" />
-                            </td>
-                        </tr>
-                    </table>
-                </form>
+                <React.Fragment>
+                    <form
+                        action=""
+                        method="POST"
+                        onSubmit={(e) => this.handleSubmit(e)}
+                        className="form"
+                    >
+                        <table className="table">
+                            <tr>
+                                <td> network_sec_type</td>
+                                <td>
+                                    <input
+                                        type="text"
+                                        name="network_sec_type"
+                                        value={this.state.taskVariables.network_sec_type.value}
+                                        readOnly="readonly"
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> node_status</td>
+                                <td>
+                                    <input
+                                        type="text"
+                                        name="node_status"
+                                        value={this.state.taskVariables.node_status.value}
+                                        readOnly="readonly"
+                                    />
+                                </td>
+                            </tr>
+
+                            {(this.state.additionalVars && this.state.additionalVars.length > 0) ? this.state.additionalVars.map((item, index) => {
+                                return (
+                                    <React.Fragment>
+                                        <tr>
+                                            <td><input type="text" placeholder="enter variable name" onBlur={(e) => this.handleKeyChange(e, index)} /></td>
+                                            <td><input type="text" placeholder="enter variable value" onBlur={(e) => this.handleValueChange(e, index)} /></td>
+                                            <td><button onClick={(e) => this.removeVariable(e, index)}>Remove Variable</button></td>
+                                        </tr>
+                                    </React.Fragment>
+                                )
+                            }) : ""}
+                            <br /><br />
+                            <button onClick={(e) => this.addVariable(e)}> Add Variable</button>
+                            <br />
+                            <tr>
+                                <td />
+                                <td>
+                                    <input type="submit" value="Submit" />
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </React.Fragment>
+
             </div>
         )
     }
